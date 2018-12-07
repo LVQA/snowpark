a1039077795e093564fa4a06c6fabda8c6e5e7bf
Quang An <ladvodoi@gmail.com>
2018-12-07 19:35:41 +0700

    update header and footer second time

--- model.js
+++ model.js
@@ -1,10 +1,9 @@
+(function() {
 var homePage = angular.module('homePage', []);
 
 homePage.controller('backTT', ['$scope','$location','$anchorScroll', function($scope,$location,$anchorScroll){
 	$scope.gototop = function(){
-		//đặt giá trị cho hash
-		$location.hash('backTT');
-		$anchorScroll;
-
+		
 	};
 }]);
+})();
